3e5ece872d0f7ece02b733049f8422511e05a469
LieutPaul <Vikas.Kalyanapuram@iiitb.ac.in>
2023-02-09 15:53:28 +0530

    Creating separate component for all Contexts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from  'react';
-// import ClassContextComponent from './ClassContextComponent';
 import FunctionContextComponent from './FunctionContextComponent';
 import ThemeProvider from './ThemeContext';
 import './App.css';
@@ -11,5 +10,3 @@ export default function App() {
     </ThemeProvider>
   );
 }
-// Context provider : All Code that requires access to a value should be within the context provider
-// Provider has a single prop called value which is made accessible to all functions within 
--- src/ThemeContext.jsx
+++ src/ThemeContext.jsx
@@ -4,6 +4,7 @@ export const ThemeContext = React.createContext();
 export const ThemeUpdateContext = React.createContext();
 
 export default function ThemeProvider({ children }){
+    
     const [darkTheme,setDarkTheme] = React.useState(true);
   
     function toggleTheme(){
@@ -20,3 +21,6 @@ export default function ThemeProvider({ children }){
         </ThemeUpdateContext.Provider>
     );
 }
+
+// Context provider : All Code that requires access to a value should be within the context provider
+// Provider has a single prop called value which is made accessible to all functions within 
